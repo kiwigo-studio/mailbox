e6cd551023c3a922280acc18aff05f989f38af35
Lewis Liu <lulusabeetw@gmail.com>
2024-05-04 15:00:21 +0800

    Add CssBaseline component for global styling in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css';
 import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CssVarsProvider defaultMode="dark">
+      <CssBaseline />
       <Component {...pageProps} />
     </CssVarsProvider>
   );
