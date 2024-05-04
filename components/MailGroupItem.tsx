ba6f65f5e2a00f2a516c5afbc3471ed1a1960e51
Lewis Liu <lulusabeetw@gmail.com>
2024-05-04 15:08:30 +0800

    Refactor MailGroupItem and MailList components to improve UI and functionality

--- components/MailGroupItem.tsx
+++ components/MailGroupItem.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { EmailGroup } from '@/models/email';
 import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
 import Typography from '@mui/joy/Typography';
 
 type Props = {
@@ -11,28 +10,26 @@ type Props = {
 
 export default function MailGroupItem({ emailGroup, isSelected }: Props) {
   return (
-    <Button variant="plain" sx={{ p: 0, width: '100%' }}>
-      <Box flexDirection="column" sx={{ px: 2, py: 0.5, width: '100%', display: 'flex' }}>
-        <Typography level="title-sm" fontWeight="600" textAlign="left" noWrap>
-          {emailGroup.fromName}
-        </Typography>
-        <Typography level="body-xs" textColor="white" textAlign="left" noWrap>
-          {emailGroup.subject}
-        </Typography>
-        <Typography
-          sx={{
-            display: '-webkit-box',
-            overflow: 'hidden',
-            WebkitBoxOrient: 'vertical',
-            WebkitLineClamp: 2,
-            height: '36px',
-          }}
-          level="body-xs"
-          textAlign="left"
-        >
-          {emailGroup.text}
-        </Typography>
-      </Box>
-    </Button>
+    <Box flexDirection="column" sx={{ px: 2, py: 0.75, width: '100%', display: 'flex' }}>
+      <Typography level="title-sm" fontWeight="600" textAlign="left" noWrap>
+        {emailGroup.fromName}
+      </Typography>
+      <Typography level="body-xs" textColor="white" textAlign="left" noWrap>
+        {emailGroup.subject}
+      </Typography>
+      <Typography
+        sx={{
+          display: '-webkit-box',
+          overflow: 'hidden',
+          WebkitBoxOrient: 'vertical',
+          WebkitLineClamp: 2,
+          height: '36px',
+        }}
+        level="body-xs"
+        textAlign="left"
+      >
+        {emailGroup.text}
+      </Typography>
+    </Box>
   );
 }
--- components/MailList.tsx
+++ components/MailList.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { EmailGroup } from '@/models/email';
 import MailGroupItem from './MailGroupItem';
 import Sheet from '@mui/joy/Sheet';
 import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
 
 type Props = {
   emailGroups: EmailGroup[];
 };
 
 export default function MailList({ emailGroups }: Props) {
+  const [selectedEmailID, setSelectedEmailID] = useState<string>('');
+  const isSelected = (emailID: string) => emailID === selectedEmailID;
+
   return (
     <Sheet
       sx={{
@@ -23,7 +27,14 @@ export default function MailList({ emailGroups }: Props) {
     >
       <Box sx={{ pt: 0.5, pb: 1 }}>
         {emailGroups.map(group => (
-          <MailGroupItem key={group.messageID} emailGroup={group} isSelected={false} />
+          <Button
+            key={group.messageID}
+            variant={isSelected(group.messageID) ? 'solid' : 'plain'}
+            sx={{ p: 0, width: '100%' }}
+            onClick={() => setSelectedEmailID(group.messageID)}
+          >
+            <MailGroupItem key={group.messageID} emailGroup={group} isSelected={isSelected(group.messageID)} />
+          </Button>
         ))}
       </Box>
     </Sheet>
