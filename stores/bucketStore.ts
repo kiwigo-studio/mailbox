5dc2274a0339793ffb6772759430b02d5a0e1499
Lewis Liu <lulusabeetw@gmail.com>
2024-05-05 17:46:13 +0800

    Refactor bucketStore to use SWR Global State for persisting bucket data (#why: simplify data management and improve scalability)

--- stores/bucketStore.ts
+++ stores/bucketStore.ts
@@ -1,8 +1,24 @@
 import { createStore } from 'swr-global-state';
+import type { StatePersistor, StateKey } from 'swr-global-state';
 import { Bucket } from '@/models/bucket';
-import { loadConfig } from '@/utils/ConfigSaver';
+
+const withLocalStoragePersistor = <T = Bucket[]>(): StatePersistor<T> => ({
+  onSet(key: StateKey, data: T) {
+    const stringifyData = JSON.stringify(data);
+    window.localStorage.setItem(String(key), stringifyData);
+  },
+  onGet(key: StateKey) {
+    const cachedData = window.localStorage.getItem(String(key)) ?? '[]';
+    try {
+      return JSON.parse(cachedData);
+    } catch {
+      return cachedData;
+    }
+  },
+});
 
 export const useBucketsStore = createStore<Bucket[]>({
   key: '@app/buckets',
-  initial: loadConfig(),
+  initial: [],
+  persistor: withLocalStoragePersistor(),
 });
