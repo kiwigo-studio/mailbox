5279f6a0f82d42db8adfdb71f3919cf9398e72ec
Lewis Liu <lulusabeetw@gmail.com>
2024-05-05 19:57:55 +0800

    Add useEffect hooks to fetch and process emails from buckets.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,57 @@
-import React, { useState } from 'react';
-import Sidebar from '@/components/Sidebar';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/joy/Box';
+import Sidebar from '@/components/Sidebar';
 import MailList from '@/components/MailList';
 import Content from '@/components/Content';
-import { S3Client } from '@aws-sdk/client-s3';
-import { fetchS3Items } from '@/utils/ReadFromS3Bucket';
-import { parseEmail } from '@/utils/ParseEmail';
-import { EmailGroup, newEmailGroupList } from '@/models/email';
-import { ParsedMail } from 'mailparser';
+import { EmailGroup } from '@/models/email';
+import { Bucket, BucketData } from '@/models/bucket';
+import { useBucketsStore } from '@/stores/bucketStore';
+import { fetchAndProcessEmails } from '@/utils/FetchAndProcessEmails';
 
 export default function Home() {
+  const [buckets] = useBucketsStore();
+  const [bucketData, setBucketData] = useState<BucketData>({});
+  const [selectedBucketId, setSelectedBucketId] = useState('');
   const [emailGroups, setEmailGroups] = useState<EmailGroup[]>([]);
+  const [selectedEmailGroup, setSelectedEmailGroup] = useState<EmailGroup | null>(null);
 
-  React.useEffect(() => {
-    const client = new S3Client({
-      region: 'ap-northeast-1',
-      credentials: {
-        accessKeyId: '',
-        secretAccessKey: '',
-      },
+  useEffect(() => {
+    const newBucketData: BucketData = {};
+    let fetchPromises: Promise<[Bucket, EmailGroup[]]>[] = [];
+    buckets.forEach(bucket => {
+      if (bucketData[bucket.id]) return;
+      fetchPromises.push(fetchAndProcessEmails(bucket));
     });
 
-    fetchS3Items(client, 'youthgoto-email').then(async items => {
-      const emailBodies = await Promise.all(items.map(async item => await item?.Body?.transformToString()));
-      const parsedEmails = await Promise.all(
-        emailBodies
-          .reduce((acc: string[], item) => {
-            item && acc.push(item);
-            return acc;
-          }, [])
-          .map(async body => await parseEmail(body)),
-      )
-        .then(emails =>
-          emails.reduce((acc: ParsedMail[], item) => {
-            item && acc.push(item);
-            return acc;
-          }, []),
-        )
-        .then(emails => newEmailGroupList(emails));
+    if (!fetchPromises.length) return;
 
-      console.log(parsedEmails);
-      setEmailGroups(parsedEmails);
+    Promise.all(fetchPromises).then(results => {
+      results.forEach(([bucket, emailGroup]) => {
+        newBucketData[bucket.id] = emailGroup;
+      });
+      setBucketData(pre => ({ ...pre, ...newBucketData }));
     });
-  }, []);
+  }, [buckets, bucketData]);
+
+  useEffect(() => {
+    const selectedBucketEmailGroups = bucketData[selectedBucketId];
+    if (selectedBucketEmailGroups) {
+      setEmailGroups(selectedBucketEmailGroups);
+    } else {
+      setEmailGroups([]);
+    }
+    setSelectedEmailGroup(null);
+  }, [buckets, bucketData, selectedBucketId]);
+
+  const handleBucketSelect = (bucketId: string) => setSelectedBucketId(bucketId);
+  const handleEmailGroupSelect = (emailGroup: EmailGroup) => setSelectedEmailGroup(emailGroup);
 
   return (
     <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
-      <Sidebar />
-      <MailList emailGroups={emailGroups} />
+      <Sidebar selectedBucketId={selectedBucketId} selectBucket={handleBucketSelect} />
+      <MailList emailGroups={emailGroups} onSelectEmailGroup={handleEmailGroupSelect} />
       <div style={{ backgroundColor: 'black', height: '100vh', width: 1 }} />
-      <Content />
+      <Content emails={selectedEmailGroup?.emails} />
     </Box>
   );
 }
