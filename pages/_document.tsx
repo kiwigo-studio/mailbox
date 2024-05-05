d9f2acae98b84926ece85d21c838f2860e4b4984
Lewis Liu <lulusabeetw@gmail.com>
2024-05-05 21:51:24 +0800

    Update favicon URL and add meta tags for improved SEO and accessibility.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const iconUrl = 'https://raw.githubusercontent.com/kiwigo-studio/mailbox/master/public/favicon.png';
   return (
     <Html lang="en">
       <Head>
         <meta charSet="utf-8" />
         <meta name="description" content="Best mailbox for AWS SES + s3 bucket." />
+        <meta name="keyWords" content="S3,AWS,Bucket,Mail,Mailbox,SES" />
+        <meta property="og:title" content="Kiwigo Mailbox" />
+        <meta property="og:description" content="Best mailbox for AWS SES + s3 bucket" />
+        <meta property="og:type" content="website" />
         <meta name="google-site-verification" content="XkD9pdjpDfPgzC-Tvw2amJEl1Z_z6tw7YSHfo1Y8BTc" />
-        <link
-          rel="icon"
-          type="image/png"
-          href="https://raw.githubusercontent.com/kiwigo-studio/mailbox/master/public/favicon.png"
-        ></link>
+        <link rel="apple-touch-icon" href={iconUrl} />
+        <link rel="icon" type="image/png" href={iconUrl} />
       </Head>
       <body>
         <Main />
