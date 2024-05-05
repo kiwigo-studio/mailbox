a55592c1efb7b606a2494f427535872dbe6acc8a
Lewis Liu <lulusabeetw@gmail.com>
2024-05-05 15:51:32 +0800

    Restructure bucket management with new store and hooks

--- models/bucket.ts
+++ models/bucket.ts
@@ -1,15 +1,17 @@
+import z from 'zod';
 import { EmailGroup } from './email';
+import { randomUUID } from 'crypto';
 
-type Credentials = {
-  accessKeyId: string;
-  secretAccessKey: string;
-};
+export const BucketSchema = z.object({
+  id: z.string().default(randomUUID),
+  name: z.string(),
+  region: z.string(),
+  credentials: z.object({
+    accessKeyId: z.string(),
+    secretAccessKey: z.string(),
+  }),
+});
 
-export type Bucket = {
-  id: string;
-  name: string;
-  region: string;
-  credentials: Credentials;
-};
+export type Bucket = z.infer<typeof BucketSchema>;
 
 export type BucketData = { [key: string]: EmailGroup[] };
