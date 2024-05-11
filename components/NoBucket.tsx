53358f40837a81df6c874ce7e29233b012b1fda2
Lewis Liu <lulusabeetw@gmail.com>
2024-05-11 22:45:57 +0800

    Update NoBucket component to use Stack and add explore demo button

--- components/NoBucket.tsx
+++ components/NoBucket.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import Box from '@mui/joy/Box';
+import { useRouter } from 'next/router';
+import Stack from '@mui/joy/Stack';
 import Typography from '@mui/joy/Typography';
+import Divider from '@mui/joy/Divider';
+import Button from '@mui/joy/Button';
 import BucketForm from './BucketForm';
 import { Bucket } from '@/models/bucket';
+import LaunchIcon from '@mui/icons-material/LaunchRounded';
 
 export default function NoBucket({ onCreate }: { onCreate: (bucket: Bucket) => void }) {
+  const router = useRouter();
+
   return (
-    <Box className="flex justify-center items-center w-full flex-col">
-      <Typography level="h3" className="mb-10">
-        Add a bucket
-      </Typography>
+    <Stack className="flex justify-center items-center w-full flex-col" spacing={2}>
+      <Typography level="h3">Add a bucket</Typography>
       <BucketForm withoutCancel editBucket={null} onSubmit={onCreate} handleClose={() => {}} />
-    </Box>
+      <Divider className="self-center" sx={{ width: '280px' }}>
+        OR
+      </Divider>
+      <Button
+        component="a"
+        color="primary"
+        variant="soft"
+        target="_blank"
+        size="sm"
+        href={router.basePath + '/demo'}
+        endDecorator={<LaunchIcon fontSize="small" />}
+        sx={{ width: '280px' }}
+      >
+        Explore Demo
+      </Button>
+    </Stack>
   );
 }
